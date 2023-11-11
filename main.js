a941fd858ec78dfa81190981585df5b3748c43f3
prinsi-t <tprincy56@gmail.com>
2023-11-11 21:59:45 +0530

    commit changes

---
+++ main.js
@@ -0,0 +1,19 @@
+/* fetch("https://dog.ceo/api/breeds/image/random")
+    .then(res => res.json())
+    .then(data => {
+        console.log(data);
+    })
+    .catch(err => {
+        console.log(`error ${err}`);
+    }); */
+
+
+fetch("https://dog.ceo/api/breeds/image/random")
+    .then(res => res.json())
+    .then(data => {
+        console.log(data.message);
+        document.querySelector('img').src = data.message
+    })
+    .catch(err => {
+        console.log(`error ${err}`);
+    });
